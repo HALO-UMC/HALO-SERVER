50924018f9d486a0d007dadce190e575e7c5dd23
SeoHyeon <shyeon16@ewha.ac.kr>
2026-06-28 22:45:21 +0900

    init: Spring Boot 초기 프로젝트 세팅

---
+++ src/main/java/com/umc/halo/HaloServerApplication.java
@@ -0,0 +1,13 @@
+package com.umc.halo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaloServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaloServerApplication.class, args);
+    }
+
+}
