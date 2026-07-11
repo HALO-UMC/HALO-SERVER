bc4560db9c4e4d0dc2d31842dd8a35b0bbd546ce
shin-jaehyun <tlswogus0225@g.hongik.ac.kr>
2026-07-11 23:54:56 +0900

    feat: refreshToken 해시 변환 함수, 생성 함수 구현

--- src/main/java/com/umc/halo/domain/member/converter/MemberConverter.java
+++ src/main/java/com/umc/halo/domain/member/converter/MemberConverter.java
@@ -1,8 +1,4 @@
 package com.umc.halo.domain.member.converter;
 
-import com.umc.halo.domain.member.dto.request.LoginRequestDTO;
-import com.umc.halo.domain.member.dto.response.LoginResponseDTO;
-
 public class MemberConverter {
-
 }
--- src/main/java/com/umc/halo/domain/member/service/MemberService.java
+++ src/main/java/com/umc/halo/domain/member/service/MemberService.java
@@ -1,7 +1,5 @@
 package com.umc.halo.domain.member.service;
 
-import com.umc.halo.domain.member.dto.request.LoginRequestDTO;
-import com.umc.halo.domain.member.dto.response.LoginResponseDTO;
 import com.umc.halo.domain.member.repository.MemberRepository;
 import com.umc.halo.global.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
@@ -13,8 +11,4 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final JwtUtil jwtUtil;
-
-    public LoginResponseDTO.LoginResponse login(LoginRequestDTO.Login dto) {
-        return null;
-    }
 }
