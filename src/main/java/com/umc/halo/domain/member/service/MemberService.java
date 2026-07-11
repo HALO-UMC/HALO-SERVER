3f05f50728e6de495bc1977687a46b909914158e
shin-jaehyun <tlswogus0225@g.hongik.ac.kr>
2026-07-11 20:23:33 +0900

    feat: MemberController.login() 구현

--- src/main/java/com/umc/halo/domain/member/service/MemberService.java
+++ src/main/java/com/umc/halo/domain/member/service/MemberService.java
@@ -1,9 +1,18 @@
 package com.umc.halo.domain.member.service;
 
+import com.umc.halo.domain.member.dto.request.LoginRequestDTO;
+import com.umc.halo.domain.member.dto.response.LoginResponseDTO;
+import com.umc.halo.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public LoginResponseDTO.LoginResponse login(LoginRequestDTO.Login dto) {
+        return null;
+    }
 }
