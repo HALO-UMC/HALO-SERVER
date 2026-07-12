94b067d0fe2b261177560b4ebb87b62d975cf313
MIN <jmjmjmin24@gmail.com>
2026-07-12 22:21:55 +0900

    feat: 온보딩 저장 요청 DTO 및 태그 조회 메서드 추가

--- src/main/java/com/umc/halo/domain/tag/repository/MemberTagRepository.java
+++ src/main/java/com/umc/halo/domain/tag/repository/MemberTagRepository.java
@@ -3,7 +3,10 @@ package com.umc.halo.domain.tag.repository;
 import com.umc.halo.domain.tag.entity.*;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.*;
+import com.umc.halo.domain.member.entity.Member;
+import java.util.List;
 
 @Repository
 public interface MemberTagRepository extends JpaRepository<MemberTag, Long> {
+    List<MemberTag> findAllByMember(Member member);
 }
