658c293242d00f1aa9ac0d2884e2a0063904b953
hhd517 <han9959e@naver.com>
2026-07-12 23:46:47 +0900

    feat: 스토리북 시작하기 Repository 쿼리 추가

--- src/main/java/com/umc/halo/domain/record/repository/MemberStorybookRepository.java
+++ src/main/java/com/umc/halo/domain/record/repository/MemberStorybookRepository.java
@@ -1,9 +1,13 @@
 package com.umc.halo.domain.record.repository;
 
+import com.umc.halo.domain.content.storybook.entity.Storybook;
+import com.umc.halo.domain.member.entity.Member;
 import com.umc.halo.domain.record.entity.*;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.*;
 
 @Repository
 public interface MemberStorybookRepository extends JpaRepository<MemberStorybook, Long> {
+
+    boolean existsByMemberAndStorybook(Member member, Storybook storybook);
 }
