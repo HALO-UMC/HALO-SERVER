6442cdaac74011d956304672b55734a9d3f81cd4
hhd517 <han9959e@naver.com>
2026-07-13 17:52:28 +0900

    feat: 스토리북 목록 조회 SuccessCode 추가

--- src/main/java/com/umc/halo/domain/content/storybook/apiPayload/StorybookSuccessCode.java
+++ src/main/java/com/umc/halo/domain/content/storybook/apiPayload/StorybookSuccessCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum StorybookSuccessCode implements BaseSuccessCode {
 
+    GET_STORYBOOK_LIST(HttpStatus.OK, "STORYBOOK200_1", "스토리북 목록을 성공적으로 조회했습니다."),
     GET_STORYBOOK_DETAIL(HttpStatus.OK, "STORYBOOK200_2", "스토리북 상세를 성공적으로 조회했습니다."),
     START_STORYBOOK(HttpStatus.CREATED, "STORYBOOK201_1", "스토리북을 성공적으로 시작했습니다.");
 
