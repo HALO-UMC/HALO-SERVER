0e1c65ba067d893950ad7574dd32ad870b2d080f
hhd517 <han9959e@naver.com>
2026-07-14 10:40:03 +0900

    fix: 스토리북 시작하기 응답 DTO 명세서에 맞게 수정 (chapterOrder -> status)

--- src/main/java/com/umc/halo/domain/content/storybook/dto/response/StorybookStartResponse.java
+++ src/main/java/com/umc/halo/domain/content/storybook/dto/response/StorybookStartResponse.java
@@ -1,10 +1,12 @@
 package com.umc.halo.domain.content.storybook.dto.response;
 
+import com.umc.halo.domain.content.storybook.enums.StorybookStatus;
+
 public class StorybookStartResponse {
 
     public record StartStorybook(
             Long memberStorybookId,
             Long storybookId,
-            int chapterOrder
+            StorybookStatus status
     ) {}
 }
