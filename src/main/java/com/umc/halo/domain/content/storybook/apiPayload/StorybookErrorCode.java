80c339d37473202f12668d00225637190a853bd4
hhd517 <han9959e@naver.com>
2026-07-14 10:38:48 +0900

    fix: 스토리북 시작하기 성공/에러 코드 명세서에 맞게 수정

--- src/main/java/com/umc/halo/domain/content/storybook/apiPayload/StorybookErrorCode.java
+++ src/main/java/com/umc/halo/domain/content/storybook/apiPayload/StorybookErrorCode.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum StorybookErrorCode implements BaseErrorCode {
 
     NOT_FOUND(HttpStatus.NOT_FOUND, "STORYBOOK404_1", "존재하지 않는 스토리북입니다."),
-    ALREADY_STARTED(HttpStatus.CONFLICT, "STORYBOOK409_2", "이미 시작한 스토리북입니다.");
+    ALREADY_IN_PROGRESS(HttpStatus.CONFLICT, "STORYBOOK409_1", "이미 시작한 스토리북입니다."),
+    ALREADY_COMPLETED(HttpStatus.CONFLICT, "STORYBOOK409_2", "이미 완료한 스토리북은 다시 시작할 수 없습니다.");
 
     private final HttpStatus status;
     private final String code;
--- src/main/java/com/umc/halo/domain/content/storybook/apiPayload/StorybookSuccessCode.java
+++ src/main/java/com/umc/halo/domain/content/storybook/apiPayload/StorybookSuccessCode.java
@@ -11,7 +11,8 @@ public enum StorybookSuccessCode implements BaseSuccessCode {
 
     GET_STORYBOOK_LIST(HttpStatus.OK, "STORYBOOK200_1", "스토리북 목록을 성공적으로 조회했습니다."),
     GET_STORYBOOK_DETAIL(HttpStatus.OK, "STORYBOOK200_2", "스토리북 상세를 성공적으로 조회했습니다."),
-    START_STORYBOOK(HttpStatus.CREATED, "STORYBOOK201_1", "스토리북을 성공적으로 시작했습니다.");
+    START_STORYBOOK(HttpStatus.OK, "STORYBOOK200_3", "스토리북을 시작했습니다."),
+    GET_RECOMMENDED_STORYBOOKS(HttpStatus.OK, "STORYBOOK200_4", "추천 스토리북을 성공적으로 조회했습니다.");
 
     private final HttpStatus status;
     private final String code;
