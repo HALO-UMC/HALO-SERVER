552b4bae4f6000fb6d3969803972ebbc3351cfb9
MIN <jmjmjmin24@gmail.com>
2026-07-15 17:31:01 +0900

    docs: 관계 정보 조회 API Swagger 문서 추가

--- src/main/java/com/umc/halo/domain/relationship/controller/RelationshipController.java
+++ src/main/java/com/umc/halo/domain/relationship/controller/RelationshipController.java
@@ -1,5 +1,6 @@
 package com.umc.halo.domain.relationship.controller;
 
+import com.umc.halo.domain.relationship.controller.docs.RelationshipControllerDocs;
 import com.umc.halo.domain.relationship.dto.RelationshipResDTO;
 import com.umc.halo.domain.relationship.exception.code.RelationshipSuccessCode;
 import com.umc.halo.domain.relationship.service.RelationshipService;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/relationships")
-public class RelationshipController {
+public class RelationshipController implements RelationshipControllerDocs {
     private final RelationshipService relationshipService;
     @GetMapping
     public ApiResponse<RelationshipResDTO.Info> getRelationshipInfo(
