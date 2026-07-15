9d4f2cc5d37d56ad5b2a52e48b49bdcf71ff8250
SeoHyeon <shyeon16@ewha.ac.kr>
2026-07-15 23:52:38 +0900

    fix: 완료된 장 다시보기 답변 조회 N+1 방지 쿼리 커밋 누락 수정

--- src/main/java/com/umc/halo/domain/record/repository/MemberChapterAnswerRepository.java
+++ src/main/java/com/umc/halo/domain/record/repository/MemberChapterAnswerRepository.java
@@ -2,14 +2,26 @@ package com.umc.halo.domain.record.repository;
 
 import com.umc.halo.domain.record.entity.*;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.*;
 import org.springframework.stereotype.*;
 
 import java.util.*;
 
 @Repository
 public interface MemberChapterAnswerRepository extends JpaRepository<MemberChapterAnswer, Long> {
+
     List<MemberChapterAnswer> findByMemberChapter(MemberChapter memberChapter);
 
+    @Query(
+            """
+                    SELECT mca FROM MemberChapterAnswer mca
+                    JOIN FETCH mca.chapterQuestion cq
+                    WHERE mca.memberChapter=:memberChapter
+                    ORDER BY cq.questionOrder ASC
+                    """)
+    List<MemberChapterAnswer> findAllByMemberChapterOrderByQuestionOrder(@Param("memberChapter") MemberChapter memberChapter);
+
     void deleteAllByMemberChapter(MemberChapter memberChapter);
+
     void deleteByMemberChapterMemberId(Long memberId);
 }
