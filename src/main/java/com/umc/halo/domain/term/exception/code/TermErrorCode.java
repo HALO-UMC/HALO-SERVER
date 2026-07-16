30042706e5a40a3b38428896db0c0fed3700a24d
MIN <jmjmjmin24@gmail.com>
2026-07-16 16:44:48 +0900

    feat: 약관 성공·에러 코드 및 예외 클래스 추가

---
+++ src/main/java/com/umc/halo/domain/term/exception/code/TermErrorCode.java
@@ -0,0 +1,23 @@
+package com.umc.halo.domain.term.exception.code;
+
+import com.umc.halo.global.apiPayload.code.BaseErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum TermErrorCode implements BaseErrorCode {
+
+    REQUIRED_NOT_AGREED(HttpStatus.BAD_REQUEST,
+            "TERMS400_1",
+            "필수 약관에 모두 동의해야 합니다."),
+    NOT_FOUND(HttpStatus.NOT_FOUND,
+            "TERMS404_1",
+            "존재하지 않는 약관입니다.")
+    ;
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+}
