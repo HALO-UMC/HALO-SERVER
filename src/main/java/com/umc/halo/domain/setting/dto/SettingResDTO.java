e3d931846d0ea0ef48081dd310dbaa9cb780ae42
shin-jaehyun <tlswogus0225@g.hongik.ac.kr>
2026-07-21 20:37:04 +0900

    feat: BGM 목록 조회 관련 DTO, Converter 추가

--- src/main/java/com/umc/halo/domain/setting/dto/SettingResDTO.java
+++ src/main/java/com/umc/halo/domain/setting/dto/SettingResDTO.java
@@ -3,6 +3,7 @@ package com.umc.halo.domain.setting.dto;
 import lombok.Builder;
 
 import java.time.LocalTime;
+import java.util.List;
 
 public class SettingResDTO {
 
@@ -14,5 +15,18 @@ public class SettingResDTO {
             Boolean retentionNotificationEnabled,
             Boolean anniversaryNotificationEnabled,
             Boolean isAllNotificationEnabled
+    ) {}
+
+    @Builder
+    public record BgmInfo(
+            Long bgmId,
+            String title,
+            String fileName,
+            String imgName
+    ) {}
+
+    @Builder
+    public record Bgms(
+            List<BgmInfo> bgms
     ) {}
 }
