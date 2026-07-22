de095db0912fc5fba7b573d6227710e114b58ff9
shin-jaehyun <tlswogus0225@g.hongik.ac.kr>
2026-07-22 19:12:36 +0900

    feat: 알림 설정 수정 Request DTO 추가

--- src/main/java/com/umc/halo/domain/setting/dto/SettingReqDTO.java
+++ src/main/java/com/umc/halo/domain/setting/dto/SettingReqDTO.java
@@ -1,4 +1,26 @@
 package com.umc.halo.domain.setting.dto;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+
+import java.time.LocalTime;
+
 public class SettingReqDTO {
+
+    @Builder
+    public record UpdateNotificationSettings(
+            @NotNull
+            Boolean regularNotificationEnabled,
+
+            LocalTime regularNotificationTime,
+
+            @NotNull
+            Boolean todayChapterNotificationEnabled,
+
+            @NotNull
+            Boolean retentionNotificationEnabled,
+
+            @NotNull
+            Boolean anniversaryNotificationEnabled
+    ) {}
 }
