f2df9db2d02712b5f271b98f5afe79a6af4cd23f
shin-jaehyun <tlswogus0225@g.hongik.ac.kr>
2026-07-26 03:37:58 +0900

    refactor: MemberSetting 엔티티 수정 (regularNotificationEnabled 컬럼 삭제)

--- src/main/java/com/umc/halo/domain/setting/service/SettingService.java
+++ src/main/java/com/umc/halo/domain/setting/service/SettingService.java
@@ -64,7 +64,6 @@ public class SettingService {
         boolean previousAnniversaryEnabled = memberSetting.getAnniversaryNotificationEnabled();
 
         memberSetting.updateNotificationSettings(
-                dto.regularNotificationEnabled(),
                 dto.regularNotificationTime(),
                 dto.todayChapterNotificationEnabled(),
                 dto.retentionNotificationEnabled(),
