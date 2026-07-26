f70bb94a43699b38f40665311933c089f6571bc7
shin-jaehyun <tlswogus0225@g.hongik.ac.kr>
2026-07-27 04:04:13 +0900

    fix: CI/CD 에러 수정(마이그레이션 스크립트 수정)

--- src/main/resources/db/migration/V4__alter_notification_table.sql
+++ src/main/resources/db/migration/V4__alter_notification_table.sql
@@ -1,15 +1,35 @@
 ALTER TABLE notification
-    ADD COLUMN setting_enabled BOOLEAN NOT NULL DEFAULT TRUE;
-
-ALTER TABLE notification
-    ADD COLUMN anniversary_enabled BOOLEAN NOT NULL DEFAULT TRUE;
+    ADD COLUMN setting_enabled BOOLEAN NULL;
 
 UPDATE notification
-    SET status = 'EXPIRED'
-    WHERE status = 'CANCELED';
+SET setting_enabled = TRUE
+WHERE setting_enabled IS NULL;
+
+ALTER TABLE notification
+    MODIFY COLUMN setting_enabled BOOLEAN NOT NULL;
+
+ALTER TABLE notification
+    ADD COLUMN anniversary_enabled BOOLEAN NULL;
+
+UPDATE notification
+SET anniversary_enabled = TRUE
+WHERE anniversary_enabled IS NULL;
+
+ALTER TABLE notification
+    MODIFY COLUMN anniversary_enabled BOOLEAN NOT NULL;
+
+UPDATE notification
+SET status = 'EXPIRED'
+WHERE status = 'CANCELED';
+
+ALTER TABLE notification
+    DROP FOREIGN KEY FK_NOTIFICATION_ON_ANNIVERSARY;
 
 ALTER TABLE notification
     DROP INDEX uk_notification_anniversary_type;
 
 ALTER TABLE notification
     ADD CONSTRAINT uk_notification_anniversary_type_scheduled_at UNIQUE (anniversary_id, notification_type, scheduled_at);
+
+ALTER TABLE notification
+    ADD CONSTRAINT FK_NOTIFICATION_ON_ANNIVERSARY FOREIGN KEY (anniversary_id) REFERENCES anniversary (anniversary_id);
