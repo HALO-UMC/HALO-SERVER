462fac84d8352b9ad5d609590721b3970853e41d
jungmin <jmjmjmin24@gmail.com>
2026-08-03 00:03:46 +0900

    chore: 비동기 스레드풀 설정 외부화 및 Executor 빈 등록

--- src/main/java/com/umc/halo/global/config/AsyncConfig.java
+++ src/main/java/com/umc/halo/global/config/AsyncConfig.java
@@ -2,8 +2,35 @@ package com.umc.halo.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
 @EnableAsync
 public class AsyncConfig {
+    @Bean
+    public ThreadPoolTaskExecutor asyncExecutor(
+            @Value("${async.executor.core-size}") int coreSize,
+            @Value("${async.executor.max-size}") int maxSize,
+            @Value("${async.executor.queue-capacity}") int queueCapacity,
+            @Value("${async.executor.keep-alive-seconds}") int keepAliveSeconds,
+            @Value("${async.executor.await-termination-seconds}") int awaitTerminationSeconds
+    ) {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(coreSize);
+        executor.setMaxPoolSize(maxSize);
+        executor.setQueueCapacity(queueCapacity);
+        executor.setKeepAliveSeconds(keepAliveSeconds);
+        executor.setThreadNamePrefix("halo-async-");
+
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setAwaitTerminationSeconds(awaitTerminationSeconds);
+
+        return executor;
+    }
+
 }
