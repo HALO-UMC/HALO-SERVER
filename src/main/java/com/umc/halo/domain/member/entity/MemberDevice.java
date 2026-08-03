24c7367afb00abf1e8d5d764e5fcf905c42f4d90
shin-jaehyun <tlswogus0225@g.hongik.ac.kr>
2026-08-03 15:58:45 +0900

    feat: MemberDevice 엔티티, DeviceType enum 추가

---
+++ src/main/java/com/umc/halo/domain/member/entity/MemberDevice.java
@@ -0,0 +1,37 @@
+package com.umc.halo.domain.member.entity;
+
+import com.umc.halo.domain.member.enums.DeviceType;
+import com.umc.halo.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "member_device",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uk_member_device", columnNames = {"member_id", "device_identifier"})
+        })
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class MemberDevice extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_device_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(name = "fcm_token", nullable = false, length = 255)
+    private String fcmToken;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "device_type", nullable = false)
+    private DeviceType deviceType;
+
+    @Column(name = "device_identifier", nullable = false, length = 255)
+    private String deviceIdentifier;
+}
