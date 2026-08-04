fdbbb8f78bc905549df1b0bc472783a6a3738b2f
shin-jaehyun <tlswogus0225@g.hongik.ac.kr>
2026-08-04 13:34:26 +0900

    save current work

--- src/main/resources/db/migration/V19__add_character_member.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE member
-    ADD COLUMN storybook_character_id BIGINT NULL AFTER member_id;
--- src/main/resources/db/migration/V20__add_member_character_constraint.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE member
-    ADD CONSTRAINT FK_MEMBER_ON_STORYBOOK_CHARACTER
-        FOREIGN KEY (storybook_character_id) REFERENCES storybook_character (storybook_character_id);
