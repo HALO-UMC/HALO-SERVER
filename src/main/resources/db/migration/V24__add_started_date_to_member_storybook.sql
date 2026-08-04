28f869aee62914d928bdebaf816e5f3766356695
hhd517 <han9959e@naver.com>
2026-08-05 00:39:00 +0900

    fix: started_date 마이그레이션 backfill 방식으로 수정, 추천 API 문서 문구 수정

--- src/main/resources/db/migration/V24__add_started_date_to_member_storybook.sql
+++ src/main/resources/db/migration/V24__add_started_date_to_member_storybook.sql
@@ -1,2 +1,9 @@
 ALTER TABLE member_storybook
-    ADD COLUMN started_date DATE NOT NULL DEFAULT (CURRENT_DATE);
+    ADD COLUMN started_date DATE NULL;
+
+UPDATE member_storybook
+SET started_date = DATE(created_at)
+WHERE started_date IS NULL;
+
+ALTER TABLE member_storybook
+    MODIFY COLUMN started_date DATE NOT NULL;
