bf9b334945a940596e9d7e610e07629bb12ca6bc
shin-jaehyun <tlswogus0225@g.hongik.ac.kr>
2026-08-08 02:52:45 +0900

    refactor: 코드래빗 리뷰 요소 수정

--- src/main/java/com/umc/halo/domain/notification/service/FcmService.java
+++ src/main/java/com/umc/halo/domain/notification/service/FcmService.java
@@ -14,7 +14,7 @@ public class FcmService {
 
     private final MemberDeviceRepository memberDeviceRepository;
 
-    public void send(MemberDevice memberDevice, String title, String body) {
+    public boolean send(MemberDevice memberDevice, String title, String body) {
         Notification notification = Notification.builder().setTitle(title).setBody(body).build();
 
         Message message = Message.builder().setToken(memberDevice.getFcmToken()).setNotification(notification).build();
@@ -22,14 +22,16 @@ public class FcmService {
         try {
             String response = FirebaseMessaging.getInstance().send(message);
             log.info("FCM 전송 성공 : response={}", response);
+            return true;
         } catch (FirebaseMessagingException e) {
             if (e.getMessagingErrorCode() == MessagingErrorCode.UNREGISTERED) {
-                log.info("만료된 FCM 토큰 삭제 : token={}", memberDevice.getFcmToken());
+                log.info("만료된 FCM 토큰 삭제 : deviceId={}", memberDevice.getId());
                 memberDeviceRepository.delete(memberDevice);
             }
-            log.error("FCM 전송 실패 token={}", memberDevice.getFcmToken(), e);
+            log.error("FCM 전송 실패 deviceId={}, errorCode={}", memberDevice.getId(), e.getMessagingErrorCode(), e);
+            return false;
         } catch (Exception e) {
-            log.error("FCM 전송 실패 token={}", memberDevice.getFcmToken(), e);
+            log.error("FCM 전송 실패 deviceId={}", memberDevice.getId(), e);
             throw new RuntimeException(e);
         }
 
