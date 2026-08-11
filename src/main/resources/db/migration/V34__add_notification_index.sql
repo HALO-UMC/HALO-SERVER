4c35d5c2eeae290ad4ac51e78a774145e2f507a4
shin-jaehyun <tlswogus0225@g.hongik.ac.kr>
2026-08-11 23:43:18 +0900

    refactor: 마이그레이션 파일 수정

--- src/main/resources/db/migration/V34__add_notification_index.sql
+++ src/main/resources/db/migration/V34__add_notification_index.sql
@@ -1,5 +1,5 @@
-CREATE INDEX idx_notification_status_scheduled_at
-    ON notification (status, scheduled_at);
+ALTER TABLE notification
+    ADD INDEX idx_notification_status_scheduled_at (status, scheduled_at);
 
-CREATE INDEX idx_notification_status_processing_at
-    ON notification (status, processing_at);
+ALTER TABLE notification
+    ADD INDEX idx_notification_status_processing_at (status, processing_at);
