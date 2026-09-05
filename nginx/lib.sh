e0a7077603b26ef85b741ca66277b2863248ade1
SeoHyeon <shyeon16@ewha.ac.kr>
2026-09-06 01:43:09 +0900

    fix: render 동시 실행 시 충돌 방지

--- nginx/lib.sh
+++ nginx/lib.sh
@@ -1,5 +1,6 @@
 FRAGMENTS_DIR=/etc/nginx/fragments
 CONF_OUT=/etc/nginx/conf.d/app.conf
+RENDER_LOCK=/tmp/nginx-render.lock
 
 render() {
   cat "$FRAGMENTS_DIR/00-redirect.conf.template" \
@@ -10,6 +11,8 @@ render() {
 
 # 렌더 후 nginx -t로 검증, $CONF_OUT을 직접 덮어쓰므로 성공/실패만 반환
 try_full() {
-  render || return 1
-  nginx -t >/tmp/nginx-t.log 2>&1
+  (
+    flock -x 9
+    render && nginx -t >/tmp/nginx-t.log 2>&1
+  ) 9>"$RENDER_LOCK"
 }
