76ac5346faf613251a8994bafe5f3191f98ae267
SeoHyeon <shyeon16@ewha.ac.kr>
2026-09-05 23:51:10 +0900

    fix: render 실패시 종료되도록 정정

--- nginx/lib.sh
+++ nginx/lib.sh
@@ -3,13 +3,13 @@ CONF_OUT=/etc/nginx/conf.d/app.conf
 
 render() {
   cat "$FRAGMENTS_DIR/00-redirect.conf.template" \
-      "$FRAGMENTS_DIR/${APP_FRAGMENT}" > "$CONF_OUT"
-  envsubst '${DOMAIN} ${DEV_DOMAIN}' < "$CONF_OUT" > "${CONF_OUT}.rendered"
+      "$FRAGMENTS_DIR/${APP_FRAGMENT}" > "$CONF_OUT" &&
+  envsubst '${DOMAIN} ${DEV_DOMAIN}' < "$CONF_OUT" > "${CONF_OUT}.rendered" &&
   mv "${CONF_OUT}.rendered" "$CONF_OUT"
 }
 
 # 렌더 후 nginx -t로 검증, $CONF_OUT을 직접 덮어쓰므로 성공/실패만 반환
 try_full() {
-  render
+  render || return 1
   nginx -t >/tmp/nginx-t.log 2>&1
 }
